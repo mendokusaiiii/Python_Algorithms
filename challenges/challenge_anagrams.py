add1eeef8b0328d139e1da0fdb1298e46148dc11
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-01 16:37:57 -0300

    chore: anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+   first_arr = sort(list(first_string.lower()))
+   first_string = "".join(first_arr)
+   second_arr = sort(list(second_string.lower()))
+   second_string = "".join(second_arr)
+
+   if not first_string or not second_string:
+      return (first_string, second_string, False)
+   
+   return (first_string, second_string, first_string == second_string)
+
+
+def sort(letters):
+   if len(letters) <= 1:
+      return letters
+   mid = letters[0]
+   right = [letter for letter in letters[1:] if letter < mid]
+   left = [letter for letter in letters[1:] if letter >= mid]
+   return sort(right) + [mid] + sort(left)
