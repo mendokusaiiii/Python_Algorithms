d531f6d7826d83d9f0a1a1945a37ce3248362715
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-01 15:00:50 -0300

    feat: strudy schedule complete

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,3 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui.""" # começando
+    """Faça o código aqui."""
     raise NotImplementedError
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        for input, output in permanence_period:
+            if input <= target_time <= output:
+                counter += 1
+    except TypeError:
+        return None
+    return counter
+
