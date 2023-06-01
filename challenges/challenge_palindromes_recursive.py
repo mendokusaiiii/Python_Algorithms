704acb5c9d72e48048d919baf594395e6468285e
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-01 16:16:08 -0300

    chore: palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    if word[low_index] == word[high_index]:
+        if len(word) == 2:
+            return True
+        return is_palindrome_recursive(word[1:-1], 0, len(word[1:-1]) - 1)
+    else:
+        return False
+
